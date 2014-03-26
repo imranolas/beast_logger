1a1dd15bac1de69bebe4727c205fcdf16ecaa3e2
Imran Sulemanji <imranolas@gmail.com>
2014-03-26 16:31:44 +0000

    climb validation

--- app/models/climb.rb
+++ app/models/climb.rb
@@ -2,6 +2,12 @@ class Climb < ActiveRecord::Base
   belongs_to :session
   attr_accessible :difficulty, :grade, :state, :steepness, :session_id
 
+  validates :difficulty, presence: true
+  validates :grade, presence: true
+  validates :state, presence: true
+  validates :steepness, presence: true
+  validates :session_id, presence: true
+
   scope :flash, where(state: 'flash')
   scope :redpoint, where(state: 'RP')
   scope :dnf, where(state: 'fail')
