1f8153b63ec9c7cece6d6bd88bbebb4f774a59fa
Imran Sulemanji <imranolas@gmail.com>
2014-03-26 20:34:39 +0000

    ready for heroku

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
-  def after_sign_in_path_for(user)
+  def after_sign_in_path_for(resource)
     seshes_path
   end
 
-  def after_sign_up_path_for(user)
+  def after_sign_up_path_for(resource)
     seshes_path
   end
 
