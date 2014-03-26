c03c427265ff4b64bcc67ca0eb87164b6a07da1a
Imran Sulemanji <imranolas@gmail.com>
2014-03-26 10:08:06 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BeastLogger::Application.config.secret_token = '507936756656fb8f7ff1fad8389e397f5d07454c104f0fef6d4c606d2c1f6b0bc18f318e7833a20f046eaf294a91a36d65041e5e90cf88d5e43acfab44d3119b'
